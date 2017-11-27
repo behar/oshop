6dc06a86081a9ea1899b6b97a56dc096b44cd61b
Behar Haziri <bhazir@atk-ks.local>
2017-11-27 14:57:05 +0100

    section 19-282

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD7PZITaXEeWK6oFJ39osd64leA4eU0nnE",
+    authDomain: "oshop-5c8cc.firebaseapp.com",
+    databaseURL: "https://oshop-5c8cc.firebaseio.com",
+    projectId: "oshop-5c8cc",
+    storageBucket: "oshop-5c8cc.appspot.com",
+    messagingSenderId: "492177622330"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD7PZITaXEeWK6oFJ39osd64leA4eU0nnE",
+    authDomain: "oshop-5c8cc.firebaseapp.com",
+    databaseURL: "https://oshop-5c8cc.firebaseio.com",
+    projectId: "oshop-5c8cc",
+    storageBucket: "oshop-5c8cc.appspot.com",
+    messagingSenderId: "492177622330"
+  }
 };
